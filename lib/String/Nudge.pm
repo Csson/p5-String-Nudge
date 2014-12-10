721a2d7c46a64a62f3f1ffd747a15b4865281772
Erik Carlsson <info@code301.com>
2014-12-10 15:50:50 +0100

    "Init."

---
+++ lib/String/Nudge.pm
@@ -0,0 +1,28 @@
+package String::Nudge;
+
+use strict;
+use warnings;
+use 5.10.1;
+
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+String::Nudge - Short intro
+
+=head1 SYNOPSIS
+
+  use String::Nudge;
+
+=head1 DESCRIPTION
+
+String::Nudge is ...
+
+=head1 SEE ALSO
+
+=cut
