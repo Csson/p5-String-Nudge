8ae086eae8393729af802263d9619414c6388061
Erik Carlsson <info@code301.com>
2016-02-19 13:47:22 +0000

    Bump version after 1.0001

--- lib/String/Nudge.pm
+++ lib/String/Nudge.pm
@@ -6,7 +6,7 @@ package String::Nudge;
 
 # ABSTRACT: Indents all lines in a multi-line string
 # AUTHORITY
-our $VERSION = '1.0001';
+our $VERSION = '1.0002';
 
 use Exporter 'import';
 our @EXPORT = qw/nudge/;
